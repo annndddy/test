ff7a9e0981576f896cf14e8a702c2c6df12df119
Andy Lam <andyptlam@ricacorp.com>
2022-04-25 10:33:35 +0800

    test

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const value = ref()
+const options = ref([{value: 123, label: 'abc'}])
+</script>
+<template>
+  <div>
+    <el-select v-model="value" class="m-2" placeholder="Select" size="large">
+      <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+    </el-select>
+    <el-button>Default</el-button>
+    <NuxtWelcome />
+  </div>
+</template>
+<style>
+@import 'element-plus/dist/index.css';
+</style>
